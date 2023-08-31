f17abd2baf188f4722e99b06509e451939fbdd4b
yoonisabdurahman <yoonis215@gmail.com>
2023-08-31 00:18:56 -0700

    Sterotype Annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/sterotype_annotation/ConfigCourse.java
@@ -0,0 +1,11 @@
+package com.cydeo.sterotype_annotation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+public class ConfigCourse {
+
+
+}
