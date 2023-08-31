684e5bf35ba389b850582c3b05867d33f8b2fa32
yoonisabdurahman <yoonis215@gmail.com>
2023-08-31 00:32:50 -0700

    component Scan annotations

--- Spring-01-Core-IoC/src/main/java/com/cydeo/sterotype_annotation/ConfigCourse.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/sterotype_annotation/ConfigCourse.java
@@ -1,10 +1,12 @@
 package com.cydeo.sterotype_annotation;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Configuration
-@Component
+//@ComponentScan
+@ComponentScan(basePackages = "com.cydeo")
 public class ConfigCourse {
 
 
