778fe9eef3d21a40ca78620fc35f000159d865e8
yoonisabdurahman <yoonis215@gmail.com>
2023-09-04 12:05:28 -0700

    Spring Abstractions

---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
@@ -0,0 +1,13 @@
+package com.cydeo.proxy;
+
+import com.cydeo.model.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailCommentNotificationProxy implements CommentNotificationProxy{
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending notification for comment : " + comment.getText());
+
+    }
+}
