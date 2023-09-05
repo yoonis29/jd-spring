595702f69d0929afdd3decafc2eedac6aaac6a57
yoonisabdurahman <yoonis215@gmail.com>
2023-09-04 22:42:35 -0700

    eager and lazy annotations

--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
@@ -14,12 +14,12 @@ public class CydeoApp {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        CommentService cs1 = context.getBean(CommentService.class);
-        CommentService cs2 = context.getBean(CommentService.class);
-
-        System.out.println(cs1);
-        System.out.println(cs2);
-
-        System.out.println(cs1 == cs2);
+//        CommentService cs1 = context.getBean(CommentService.class);
+//        CommentService cs2 = context.getBean(CommentService.class);
+//
+//        System.out.println(cs1);
+//        System.out.println(cs2);
+//
+//        System.out.println(cs1 == cs2);
     }
 }
