f441024f20ed9301800801f7901bbe4cfb45edf6
yoonisabdurahman <yoonis215@gmail.com>
2023-10-26 01:29:48 -0700

    ORM Part-1

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-public class Employee {
+public class Employee { //employee
     @Id
     private int id;
     private String name;
