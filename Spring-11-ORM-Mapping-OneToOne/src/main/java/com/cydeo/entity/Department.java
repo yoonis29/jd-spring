f3e74ab008a716b52a032ce953abaf58c225018b
yoonisabdurahman <yoonis215@gmail.com>
2023-11-06 22:07:11 -0800

    ORM Part-3

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Department.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Department.java
@@ -1,6 +1,7 @@
 package com.cydeo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,8 @@ public class Department extends BaseEntity {
 
     private String departments;
     private String division;
+    @OneToOne(mappedBy = "department")
+    private Employee employee;
 
     public Department(String departments, String divison) {
         this.departments = departments;
--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
@@ -22,6 +22,14 @@ public class Employee extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    @OneToOne(cascade = CascadeType.ALL)
+//    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
+    @JoinColumn(name = "department_id")
+    private Department department;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
     public Employee(String firstName, String lastName, String email, LocalDate hireDate, int salary, Gender gender) {
         this.firstName = firstName;
         this.lastName = lastName;
