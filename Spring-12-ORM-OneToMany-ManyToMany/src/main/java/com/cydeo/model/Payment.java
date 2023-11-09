917cb7f6b27b45ee6bbefad93aada0638a0433fb
yoonisabdurahman <yoonis215@gmail.com>
2023-11-08 19:25:23 -0800

    ORM Part-4

--- Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Payment.java
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Payment.java
@@ -26,6 +26,8 @@ public class Payment {
     private PaymentDetail paymentDetail;
     @ManyToOne
     private Merchant merchant;
+    @ManyToOne
+    private  Customer customer;
 
     public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
         this.createdDate = createdDate;
