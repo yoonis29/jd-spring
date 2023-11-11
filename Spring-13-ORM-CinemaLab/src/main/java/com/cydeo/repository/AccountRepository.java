2aded9b2ec44e44478fafa6694cbd4db50a74fef
yoonisabdurahman <yoonis215@gmail.com>
2023-11-11 10:05:44 -0800

    Cinema repository completed

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
@@ -4,6 +4,7 @@ import com.cydeo.entity.Account;
 import com.cydeo.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -52,9 +53,16 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to read all accounts with an age lower than a specific value
+    @Query(value = "SELECT * FROM account_details WHERE age < :?1",nativeQuery = true)
+    List<Account> retrieveAllBasedOnAge(@Param("inputAge") Integer age);
 
     //Write a native query to read all accounts that a specific value can be containable in the name, address, country, state city
+    @Query(value = "SELECT * FROM account_details WHERE name like concat('%' , ?1, '%') OR" +
+            "address ILIKE concat('%', ?1, '%')OR" +
+            "country ILIKE concat('%', ?1, '%')OR" +
+            "stateILIKE concat('%', ?1, '%')OR" +
+            "city ILIKE concat('%', ?1, '%')OR", nativeQuery = true)
+    List<Account> retrieveBySearchCriteria(String pattern);
 
-    //Write a native query to read all accounts with an age lower than a specific value
 
 }
