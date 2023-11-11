a14859dcd6f5bde57eb53ad09b60389a8ef2a438
yoonisabdurahman <yoonis215@gmail.com>
2023-11-11 08:49:41 -0800

    Cinema Lab

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
@@ -1,8 +1,12 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Account;
+import com.cydeo.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
@@ -10,24 +14,40 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to list all accounts with a specific country or state
+     List<Account> findAllByCountryOrState(String country, String state);
 
     //Write a derived query to list all accounts with age lower than or equal to a specific value
+    List<Account> findAllByAgeLessThanEqual(Integer age);
 
     //Write a derived query to list all accounts with a specific role
 
+    List<Account> findAllByRole(UserRole role);
+
     //Write a derived query to list all accounts between a range of ages
+    List<Account>findAllByAgeBetween(Integer ageStart, Integer ageEnd);
 
     //Write a derived query to list all accounts where the beginning of the address contains the keyword
 
+    List<Account>findAllByAddressStartingWith(String keyword);
+
     //Write a derived query to sort the list of accounts with age
+
+    List<Account>findAllByOrderByAgeAddressDesc();
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns all accounts
+    @Query("SELECT a From Account a")
+    List<Account> fetchAllAccounts();
+
 
     //Write a JPQL query to list all admin accounts
+    @Query("SELECT a FROM Account a where a.role = 'Admin'")
+    List<Account> fetchAdminAccounts();
 
     //Write a JPQL query to sort all accounts with age
+    @Query("SELECT a FROM Account a ORDER BY a.age")
+    List<Account> fetchAllOrderByAge();
 
     // ------------------- Native QUERIES ------------------- //
 
