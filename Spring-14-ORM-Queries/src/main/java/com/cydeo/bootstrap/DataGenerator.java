2f786cfdfe01cbe8afed39b287e24ef89fbe1341
yoonisabdurahman <yoonis215@gmail.com>
2023-11-14 19:47:37 -0800

    Queries part 2

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,6 +1,7 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.repository.DepartmentRepository;
+import com.cydeo.repository.EmployeeRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,12 @@ public class DataGenerator implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
     private final DepartmentRepository departmentRepository;
+    private final EmployeeRepository employeeRepository;
 
-    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
+    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
         this.regionRepository = regionRepository;
         this.departmentRepository = departmentRepository;
+        this.employeeRepository = employeeRepository;
     }
 
 
@@ -43,6 +46,16 @@ public class DataGenerator implements CommandLineRunner {
 
 
         System.out.println("------------------DEPARTMENT END--------------------");
+
+
+        System.out.println("------------------EMPLOYEE START--------------------");
+
+        System.out.println("getEmployeeDetail:" + employeeRepository.getEmployeeDetail());
+        System.out.println("getEmployeeSalary:" + employeeRepository.getEmployeeSalary());
+
+
+        System.out.println("------------------EMPLOYEE END--------------------");
+
 
     }
 }
--- Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
@@ -41,5 +43,15 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //Display all employees that do not have email address
 
     List<Employee> findByEmailIsNull();
+    @Query("SELECT e FROM Employee e where e.email = 'sdubber7@t-online.de'")
+    Employee getEmployeeDetail();
+
+    @Query("SELECT e.salary FROM Employee e where e.email = 'sdubber7@t-online.de'")
+    Integer getEmployeeSalary();
+
+    @Query("SELECT e from Employee  e WHERE e.email=?1")
+    Optional<Employee> getEmployeeDetail(String email);
+    @Query("SELECT e from Employee  e WHERE e.email=?1 And e.salary=?2")
+    Employee getEmployeeDetail(String email, int salary);
 
 }
