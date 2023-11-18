ec2941b3756bd99c742f81c869a73912ff3f91c8
yoonisabdurahman <yoonis215@gmail.com>
2023-11-18 09:07:06 -0800

    TicketRepository started

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/TicketRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/TicketRepository.java
@@ -3,17 +3,24 @@ package com.cydeo.repository;
 import com.cydeo.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to count how many tickets a user bought
+    Integer countAllByUserAccountId(Long userId);
 
     //Write a derived query to list all tickets by specific email
+    List<Ticket> findAllByUserAccount_Email(String email);
 
     //Write a derived query to count how many tickets are sold for a specific movie
+    Integer countAllByMovieCinema_Movie_Name(String name);
 
     //Write a derived query to list all tickets between a range of dates
+    List<Ticket> findAllByDateTimeBetween(LocalDateTime dateTime, LocalDateTime dateTime2);
 
     // ------------------- JPQL QUERIES ------------------- //
 
