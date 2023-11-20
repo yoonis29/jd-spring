698ec56662c1e22ddfcff2e8854b149004e23fa1
yoonisabdurahman <yoonis215@gmail.com>
2023-11-20 00:18:02 -0800

    TicketRepository completed

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/TicketRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/TicketRepository.java
@@ -2,6 +2,7 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -25,21 +26,48 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns all tickets are bought from a specific user
+    @Query("SELECT t FROM Ticket t where t.userAccount.id= ?1")
+    List<Ticket> fetchAllTicketsByUserAccounts(Long userId);
 
     //Write a JPQL query that returns all tickets between a range of dates
+    @Query("SELECT t from Ticket t Where t.dateTime BETWEEN ?1 AND  ?2")
+    List<Ticket> fetchALlTicketsBetweenRangeOfDateTimes(LocalDateTime dateTime1, LocalDateTime dateTime2);
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to count the number of tickets a user bought
+    @Query(value = "SELECT COUNT(*) from ticket where user_account_id = ?1" , nativeQuery = true)
+    Integer countTicketsByUserInDateRange(Long userId, LocalDateTime localDateTime1);
+
+    //Write a native query to count the number of tickets a user bought
+    @Query(value = "SELECT COUNT(*) FROM ticket WHERE user_account_id = ?1",nativeQuery = true)
+    Integer countAllTicketsByUserAccount(Long userId);
 
     //Write a native query to count the number of tickets a user bought in a specific range of dates
+    @Query(value = "SELECT COUNT(*) FROM ticket WHERE user_account_id = ?1 AND date_time BETWEEN ?2 AND ?3",nativeQuery = true)
+    Integer countTicketByUserInDateRange(Long userId, LocalDateTime dateTime1, LocalDateTime dateTime2);
 
     //Write a native query to distinct all tickets by movie name
+    @Query(value = "SELECT DISTINCT(m.name) FROM ticket t JOIN movie_cinema mc ON t.movie_cinema_id = mc.id " +
+            " JOIN movie m ON mc.movie_id = m.id",nativeQuery = true)
+    List<String> retrieveAllDistinctMovieName();
 
     //Write a native query to find all tickets by user email
+    @Query(value = "SELECT * FROM ticket t JOIN user_account ua ON t.user_account_id = ua.id " +
+            " WHERE ua.email = ?1",nativeQuery = true)
+    List<Ticket> findAllByUserEmail(String email);
 
     //Write a native query that returns all tickets
+    @Query(value = "SELECT * FROM ticket",nativeQuery = true)
+    List<Ticket> retrieveAll();
 
-    //Write a native query to list all tickets where a specific value should be containable in the username or name or movie name
+    //Write a native query to list all tickets where a specific value should be containable in the username or account name or movie name
+
+    @Query(value = "SELECT * FROM ticket t JOIN user_account ua ON t.user_account_id = ua.id " +
+            " JOIN account_details ad ON ua.account_details_id = ad.id JOIN movie_cinema mc " +
+            " ON t.movie_cinema_id = mc.id JOIN movie m ON mc.movie_id = m.id " +
+            " WHERE ua.username ILIKE concat('%',?1,'%') OR ad.name ILIKE concat('%',?1,'%') OR " +
+            " m.name ILIKE concat('%',?1,'%') ",nativeQuery = true)
+    List<Ticket> retrieveAllBySearchCriteria(String keyword);
 
 }
